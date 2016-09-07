e74bbca052d1699b0342609cf7972f2d6f2f9881
tomerg <tomergoldsmith@Tomers-MacBook-Pro.local>
2016-09-07 10:01:57 +0300

    Added heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var mongoose = require('mongoose');
 
-mongoose.connect('mongodb://localhost/rereddit');
+// mongoose.connect('mongodb://localhost/rereddit');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
@@ -11,6 +11,8 @@ var app = express();
 
 app.use(express.static('public'));
 app.use(express.static('node_modules'));
+
+mongoose.connect(process.env.MONGOLAB_YELLOW_URI || 'mongodb://localhost/rereddit-dev');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
